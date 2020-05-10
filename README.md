037daf00c4ed847a99fb1bfd5969f806f7fd0c7b
Peter Evans <peter-evans@users.noreply.github.com>
2020-05-10 17:34:14 +0900

    Add action

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# create-or-update-project-card
-A GitHub action to create or update a project card
+# Create or Update Project Card
+[![CI](https://github.com/peter-evans/create-or-update-project-card/workflows/CI/badge.svg)](https://github.com/peter-evans/create-or-update-project-card/actions?query=workflow%3ACI)
+[![GitHub Marketplace](https://img.shields.io/badge/Marketplace-Create%20or%20Update%20Project%20Card-blue.svg?colorA=24292e&colorB=0366d6&style=flat&longCache=true&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAYAAAAfSC3RAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAM6wAADOsB5dZE0gAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAERSURBVCiRhZG/SsMxFEZPfsVJ61jbxaF0cRQRcRJ9hlYn30IHN/+9iquDCOIsblIrOjqKgy5aKoJQj4O3EEtbPwhJbr6Te28CmdSKeqzeqr0YbfVIrTBKakvtOl5dtTkK+v4HfA9PEyBFCY9AGVgCBLaBp1jPAyfAJ/AAdIEG0dNAiyP7+K1qIfMdonZic6+WJoBJvQlvuwDqcXadUuqPA1NKAlexbRTAIMvMOCjTbMwl1LtI/6KWJ5Q6rT6Ht1MA58AX8Apcqqt5r2qhrgAXQC3CZ6i1+KMd9TRu3MvA3aH/fFPnBodb6oe6HM8+lYHrGdRXW8M9bMZtPXUji69lmf5Cmamq7quNLFZXD9Rq7v0Bpc1o/tp0fisAAAAASUVORK5CYII=)](https://github.com/marketplace/actions/create-or-update-project-card)
+
+A GitHub action to create or update a project card.
+
+## Usage
+
+### Create a project card
+
+```yml
+      - name: Create or Update Project Card
+        uses: peter-evans/create-or-update-project-card@v1
+        with:
+          project-name: My project
+          column-name: My column
+          issue-number: 1
+```
+
+### Update a project card
+
+If a card already exists in project `My project` for issue `1`, the action will check if the card is in column `My second column`.
+If not in the specified column, the action will move the card.
+
+```yml
+      - name: Create or Update Project Card
+        uses: peter-evans/create-or-update-project-card@v1
+        with:
+          project-name: My project
+          column-name: My second column
+          issue-number: 1
+```
+
+### Create a card for all new issues
+
+```yml
+on:
+  issues:
+    types: [opened]
+jobs:
+  createCard:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Create or Update Project Card
+        uses: peter-evans/create-or-update-project-card@v1
+        with:
+          project-name: My project
+          column-name: My column
+```
+
+### Create a card for all new pull requests
+
+```yml
+on:
+  pull_request:
+    types: [opened]
+jobs:
+  createCard:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Create or Update Project Card
+        uses: peter-evans/create-or-update-project-card@v1
+        with:
+          project-name: My project
+          column-name: My column
+          issue-number: ${{ github.event.number }}
+```
+
+### Action inputs
+
+| Name | Description | Default |
+| --- | --- | --- |
+| `token` | `GITHUB_TOKEN` or a `repo` scoped [PAT](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line). | `GITHUB_TOKEN` |
+| `project-number` | (**semi-required**) The number of the project. Either `project-number` OR `project-name` must be supplied. | |
+| `project-name` | (**semi-required**) The name of the project. Either `project-number` OR `project-name` must be supplied. Note that a project's name is not unique. The action will use the first matching project found. | |
+| `column-name` | (**required**) The name of the column to add a card to, or move an existing card to. | |
+| `repository` | The GitHub repository containing the issue or pull request. | `github.repository` (current repository) |
+| `issue-number` | The issue or pull request number to associate with the card. | `github.event.issue.number` |
+
+### Action outputs
+
+The action outputs `card-id` for use in later workflow steps.
+
+```yml
+      - name: Create or Update Project Card
+        id: coupc
+        uses: peter-evans/create-or-update-project-card@v1
+        with:
+          project-name: My project
+          column-name: My column
+          issue-number: 1
+      - name: Check output
+        run: echo ${{ steps.coupc.outputs.card-id }}
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ src/main.ts
@@ -0,0 +1,164 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {inspect} from 'util'
+
+function getProject(projects, projectNumber, projectName): object {
+  if (!isNaN(projectNumber) && projectNumber > 0) {
+    return projects.find(project => project.number == projectNumber)
+  } else if (projectName) {
+    return projects.find(project => project.name == projectName)
+  } else {
+    throw 'A valid input for project-number OR project-name must be supplied.'
+  }
+}
+
+async function getContent(octokit, owner, repo, issueNumber): Promise<object> {
+  const {data: issue} = await octokit.issues.get({
+    owner: owner,
+    repo: repo,
+    issue_number: issueNumber
+  })
+  core.debug(`Issue: ${inspect(issue)}`)
+  if (!issue)
+    throw 'No issue or pull request matching the supplied input found.'
+
+  if (issue['pull_request']) {
+    const {data: pull} = await octokit.pulls.get({
+      owner: owner,
+      repo: repo,
+      pull_number: issueNumber
+    })
+    return {
+      id: pull['id'],
+      url: pull['url'],
+      type: 'PullRequest'
+    }
+  } else {
+    return {
+      id: issue['id'],
+      url: issue['url'],
+      type: 'Issue'
+    }
+  }
+}
+
+async function findCardInColumn(
+  octokit,
+  columnId,
+  contentUrl,
+  page = 1
+): Promise<object> {
+  const perPage = 100
+  const {data: cards} = await octokit.projects.listCards({
+    column_id: columnId,
+    per_page: perPage,
+    page: page
+  })
+  core.debug(`Cards: ${inspect(cards)}`)
+
+  const card = cards.find(card => card.content_url == contentUrl)
+
+  if (card) {
+    return card
+  } else if (cards.length == perPage) {
+    return findCardInColumn(octokit, columnId, contentUrl, ++page)
+  } else {
+    return {}
+  }
+}
+
+async function findCardInColumns(
+  octokit,
+  columns,
+  contentUrl
+): Promise<object> {
+  for (const column of columns) {
+    const card = await findCardInColumn(octokit, column['id'], contentUrl)
+    core.debug(`findCardInColumn: ${inspect(card)}`)
+    if (Object.keys(card).length > 0) {
+      return card
+    }
+  }
+  return {}
+}
+
+async function run(): Promise<void> {
+  try {
+    const inputs = {
+      token: core.getInput('token'),
+      projectNumber: Number(core.getInput('project-number')),
+      projectName: core.getInput('project-name'),
+      columnName: core.getInput('column-name'),
+      repository: core.getInput('repository'),
+      issueNumber: Number(core.getInput('issue-number'))
+    }
+    core.debug(`Inputs: ${inspect(inputs)}`)
+
+    const [owner, repo] = inputs.repository.split('/')
+
+    const octokit = new github.GitHub(inputs.token)
+
+    const {data: projects} = await octokit.projects.listForRepo({
+      owner: owner,
+      repo: repo
+    })
+    core.debug(`Projects: ${inspect(projects)}`)
+
+    const project = getProject(
+      projects,
+      inputs.projectNumber,
+      inputs.projectName
+    )
+    core.debug(`Project: ${inspect(project)}`)
+    if (!project) throw 'No project matching the supplied inputs found.'
+
+    const {data: columns} = await octokit.projects.listColumns({
+      project_id: project['id']
+    })
+    core.debug(`Columns: ${inspect(columns)}`)
+
+    const column = columns.find(column => column.name == inputs.columnName)
+    core.debug(`Column: ${inspect(column)}`)
+    if (!column) throw 'No column matching the supplied input found.'
+
+    const content = await getContent(octokit, owner, repo, inputs.issueNumber)
+    core.debug(`Content: ${inspect(content)}`)
+
+    const existingCard = await findCardInColumns(
+      octokit,
+      columns,
+      content['url']
+    )
+    if (Object.keys(existingCard).length > 0) {
+      core.debug(`Existing card: ${inspect(existingCard)}`)
+      core.info(
+        `An existing card is already associated with ${content['type']} #${inputs.issueNumber}`
+      )
+      core.setOutput('card-id', existingCard['id'])
+
+      if (existingCard['column_url'] != column['url']) {
+        core.info(`Moving card to column '${inputs.columnName}'`)
+        await octokit.projects.moveCard({
+          card_id: existingCard['id'],
+          position: 'top',
+          column_id: column['id']
+        })
+      }
+    } else {
+      core.info(
+        `Creating card associated with ${content['type']} #${inputs.issueNumber}`
+      )
+      const {data: card} = await octokit.projects.createCard({
+        column_id: column['id'],
+        content_id: content['id'],
+        content_type: content['type']
+      })
+      core.setOutput('card-id', card['id'])
+    }
+  } catch (error) {
+    core.debug(inspect(error))
+    core.setFailed(error.message)
+  }
+}
+
+run()
